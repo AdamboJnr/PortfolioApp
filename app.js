1596ee37c3e3cd2c6b4ead8e269815dddbca157f
AdamboJnr <AdamboAllan75@gmail.com>
2023-01-06 18:30:05 +0300

    Created Db Connection

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const userRoute = require('./routes/portfolio')
+const connectDB = require('./db/connect')
+const mongoose = require('mongoose')
+require('dotenv').config
 
 app.use(express.json())
 
@@ -9,6 +12,25 @@ app.use('/user', userRoute)
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log(`Server listening to port: ${PORT}`);
-})
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true, 
+            useFindAndModify: true
+        })
+
+        const conn = mongoose.connection
+
+        conn.once('error', console.error.bind('error', 'Failed to connect to Database'))
+
+        conn.on('open', () => {
+            console.log('Connected to daatabase succesfully')
+        })
+
+        app.listen(PORT, () => console.log(`Server listening to port: ${PORT}`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
