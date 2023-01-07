d7f33c18f6c245b8dfc0c97a51d2eb3cf9d0558b
AdamboJnr <AdamboAllan75@gmail.com>
2023-01-07 23:35:06 +0300

    Added the logger middleware

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,9 +1,10 @@
 const router = require('express').Router()
 const { login, createUser, updateUser, deleteUser } = require('../controllers/portfolio')
 const notFound = require('../middleware/notFound')
+const logger = require('../middleware/logger')
 
-router.route('/').get(login).post(createUser)
-router.route('/:id').patch(updateUser).delete(deleteUser)
-router.use(notFound)
+router.route('/').get( logger, login ).post( logger, createUser )
+router.route('/:id').patch( logger, updateUser).delete( logger,deleteUser )
+router.use( logger,notFound )
 
 module.exports = router
